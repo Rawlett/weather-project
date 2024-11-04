baac40b48fb70021048bd44e06fe4d121b86abbb
Raúl González <rawlett17@outlook.es>
2024-11-04 17:55:10 +0100

    creacion de los environments sobre la api weather

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://api.weatherapi.com/v1/',
+    apiKey: 'key=18408f3a054b4cbd8e0122202240411',
+    apiLang: '&lang=es',
+    apiSearch: 'search.json?',
+    apiParametre: '&q='
+  };
