874757692a54d75b5f9a0df48e2df1359a76cf59
Raúl González <rawlett17@outlook.es>
2024-11-05 11:42:11 +0100

    funcionalidad buscador

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -11,6 +11,7 @@ export class SearchComponent implements OnInit {
 
   busqueda: string = ''
   filteredWeathers: WeatherInterface[] = []
+  selectedWeather?: WeatherInterface
 
   constructor(private weathersService: WeathersService) {}
 
@@ -20,7 +21,32 @@ export class SearchComponent implements OnInit {
     if (this.busqueda != '') {
       this.weathersService.getWeathersWithSearch(this.busqueda).subscribe((weathers: WeatherInterface[]) => {
         this.filteredWeathers = weathers
+        this.orderLista()
       })
+    } else {
+      this.filteredWeathers = []
     }
+  }
+
+  orderLista() {
+    this.filteredWeathers.sort((a, b) => {
+      const nameComparison = a.name.localeCompare(b.name)
+      if (nameComparison !== 0) return nameComparison
+  
+      const regionComparison = a.region.localeCompare(b.region)
+      if (regionComparison !== 0) return regionComparison
+    
+      return a.country.localeCompare(b.country)
+    })
+  }
+
+  onOptionSelected(event: any) {
+    const optionSelected = event.option.value
+
+    this.busqueda = `${optionSelected.name}, ${optionSelected.region}, ${optionSelected.country}`
+
+    this.selectedWeather = this.filteredWeathers.find(weather => weather.id === optionSelected.id)
+    
+    console.log('Objeto seleccionado:', this.selectedWeather)
   }
 }
